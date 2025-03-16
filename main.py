dc00069fbefe19de762f5cc7b74e5b04d9aa4b7b
nargissultana12 <ns0550514@gmail.com>
2025-03-16 06:57:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from flask import Flask
 import os
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
 if __name__ == "__main__":
+    # Run the bot script in a separate process
     os.system("python bot.py")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
